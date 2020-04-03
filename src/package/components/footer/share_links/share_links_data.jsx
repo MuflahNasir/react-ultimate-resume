70dbdb18bc417a0ec16b0d424f79bc3440def428
Syed Danial Munsif <danial.munsif@quantis.it>
2020-04-03 09:54:29 +0500

    Update some files

--- src/package/components/footer/share_links/share_links_data.jsx
+++ src/package/components/footer/share_links/share_links_data.jsx
@@ -5,9 +5,8 @@ import { FormattedMessage } from 'react-intl';
 import { CopyWrapper } from '../../commons/copy_wrapper/copy_wrapper';
 
 import { ReactComponent as TwitterIcon } from '../../../assets/icons/brands/twitter.svg';
-import { ReactComponent as FacebookIcon } from '../../../assets/icons/brands/facebook.svg';
 import { ReactComponent as LinkedInIcon } from '../../../assets/icons/brands/linkedin.svg';
-import { ReactComponent as ShareIcon } from '../../../assets/icons/share.svg';
+import { ReactComponent as GithubLogo } from '../../../assets/icons/brands/github.svg';
 
 export const SHARE_LINKS_DATA = Object.freeze({
     twitter: {
@@ -21,17 +20,6 @@ export const SHARE_LINKS_DATA = Object.freeze({
         ),
         getLink: ({ translatedMessage }) => `https://twitter.com/MuflahNasir`
     },
-    facebook: {
-        icon: FacebookIcon,
-        tooltipTranslation: (
-            <FormattedMessage
-                id="Footer.shareLinks.shareOnTooltip"
-                defaultMessage="Share on {platform}"
-                values={{ platform: 'Facebook' }}
-            />
-        ),
-        getLink: ({ link }) => `https://www.facebook.com/muflah.nasir`
-    },
     linkedIn: {
         icon: LinkedInIcon,
         tooltipTranslation: (
@@ -42,5 +30,16 @@ export const SHARE_LINKS_DATA = Object.freeze({
             />
         ),
         getLink: ({ link }) => `https://www.linkedin.com/in/muflah-nasir-68a0aa145/`
+    },
+    github: {
+        icon: GithubLogo,
+        tooltipTranslation: (
+            <FormattedMessage
+                id="Footer.shareLinks.shareOnTooltip"
+                defaultMessage="Share on {platform}"
+                values={{ platform: 'Github' }}
+            />
+        ),
+        getLink: ({ link }) => `https://github.com/MuflahNasir`
     }
 });
